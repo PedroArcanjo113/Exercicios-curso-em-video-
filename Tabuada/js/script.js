236bd7035577bea3bac287d072c29b857912572c
Pedro Arcanjo <pedroarcanjo113@gmail.com>
2023-07-17 15:10:51 -0300

    Tabuada

---
+++ Tabuada/js/script.js
@@ -0,0 +1,22 @@
+function res(){
+    var num = document.getElementById('num')
+    var table = document.getElementById('table')
+    
+    if(num.value.length == 0){
+        //ERRO!
+        
+        window.alert('#ERRO# Por favor, digite um número!')
+    }else{
+        //Gerador da tabuada
+        var n = Number(num.value)
+        table.innerHTML= ''
+
+        for(var c = 1; c <= 10; c++){
+            var item = document.createElement('option')
+
+           item.text += ` ${n} X ${c} = ${n*c} `
+           item.value =`tab${c}`
+           table.appendChild(item)
+        }
+    }
+}
---
+++ Tabuada/readme.md
@@ -0,0 +1,5 @@
+Olá,
+
+Esse exercício eu fiz uma tabela da calculadora, onde o usuário vai poder escolher qual número será gerado!.
+
+Vai ser mostrado a tabuada de 1 até 10
